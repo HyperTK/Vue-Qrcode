c23f1a326e9d776472766b2411ae3c2e697313d4
TK <kakeru.t0227@gmail.com>
2020-04-15 16:13:44 +0900

    first commit

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import vuetify from "./plugins/vuetify";
+import VueQrcodeReader from "vue-qrcode-reader";
+
+Vue.use(VueQrcodeReader);
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  vuetify,
+  render: h => h(App)
+}).$mount("#app");
